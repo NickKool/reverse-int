d33cca23f74ff36e6b214f4cc29b0eb7d0596679
Nikolay <nickcooluxui@gmail.com>
2025-09-08 00:29:36 +0300

    feat: add work code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(x) {
+  const numStr = x.toString();
+
+  const reversedStr = numStr.split('').reverse().join('');
+
+  const reversedNum = parseInt(reversedStr, 10);
+
+  return reversedNum;
 };
